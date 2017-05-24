9b12bf7a25889898267fa4c456a0f131e37d5000
Daniel Słaby <pufek93@gmail.com>
2017-05-24 19:19:58 +0200

    added everything up to deletion

--- app/src/main/java/com/example/slaby/android_6/Gallery.java
+++ app/src/main/java/com/example/slaby/android_6/Gallery.java
@@ -1,6 +1,8 @@
 package com.example.slaby.android_6;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
@@ -19,6 +21,11 @@ public class Gallery extends AppCompatActivity {
     File userImageFolder;
     List<File> userImagesList;
     GridLayout gridImagePreview;
+    int index = 0;
+    int numberOfImages = 9;
+    String selectedImagePath = "";
+    List<ImageView> imageViewList;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +33,50 @@ public class Gallery extends AppCompatActivity {
         setContentView(R.layout.activity_gallery);
         userImageFolder = getAlbumStorageDir(this, "Photos");
         userImagesList = getUserImagesFiles();
+        prefs = getSharedPreferences("com.example.slaby.android_6", Context.MODE_PRIVATE);
+
         gridImagePreview = (GridLayout) findViewById(R.id.gridImagePreview);
-        addImagesToGrid();
+        imageViewList = getImageViewsFromFiles(userImagesList);
+        setImagesOnGrid(index);
     }
 
-    public void addImagesToGrid() {
-        List<ImageView> imageViewList = getImageViewsFromFiles(userImagesList);
-        for (int i = 0; i < imageViewList.size(); i++) {
+    public void refreshDirectory() {
+        userImagesList = getUserImagesFiles();
+        imageViewList = getImageViewsFromFiles(userImagesList);
+    }
+
+    public void setImagesOnGrid(int index) {
+        gridImagePreview.removeAllViews();
+
+        for (int i = index; i < index + numberOfImages; i++) {
+            if (i >= imageViewList.size()) {
+                return;
+            }
             View tmp = imageViewList.get(i);
-            tmp.setPadding(10,10,10,10);
-            tmp.setMinimumWidth(400);
-            tmp.setMinimumHeight(400);
+
+            tmp.setPadding(10, 10, 10, 10);
+            tmp.setMinimumWidth(350);
+            tmp.setMinimumHeight(450);
+            tmp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ImageView imagePath = (ImageView) v;
+                    String path = imagePath.getTag().toString();
+                    savePathToPrefs(path);
+                    goBackToMain();
+                }
+            });
             gridImagePreview.addView(tmp);
         }
+    }
+
+    public void goBackToMain() {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void savePathToPrefs(String path) {
+        prefs.edit().putString("sciezka", path).apply();
     }
 
     public List<ImageView> getImageViewsFromFiles(List<File> files) {
@@ -47,7 +85,21 @@ public class Gallery extends AppCompatActivity {
             File tmp = files.get(i);
             Bitmap bitmap = BitmapFactory.decodeFile(tmp.getPath());
             ImageView tmpImageView = new ImageView(this);
+            tmpImageView.setTag(tmp.getAbsoluteFile());
             tmpImageView.setImageBitmap(bitmap);
+            tmpImageView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    ImageView imagePath = (ImageView) v;
+                    String path = imagePath.getTag().toString();
+                    File f = new File(path);
+                    f.delete();
+                    refreshDirectory();
+                    index = 0;
+                    setImagesOnGrid(index);
+                    return true;
+                }
+            });
             imageViewList.add(tmpImageView);
         }
         return imageViewList;
@@ -66,4 +118,18 @@ public class Gallery extends AppCompatActivity {
         }
         return file;
     }
+
+    public void onNext(View view) {
+        index = Math.min(imageViewList.size() - numberOfImages, index + numberOfImages);
+        setImagesOnGrid(index);
+    }
+
+    public void onPrev(View view) {
+
+        index = Math.max(0, index - numberOfImages);
+
+
+        setImagesOnGrid(index);
+    }
+
 }
--- app/src/main/java/com/example/slaby/android_6/MainActivity.java
+++ app/src/main/java/com/example/slaby/android_6/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.slaby.android_6;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -20,14 +23,48 @@ public class MainActivity extends AppCompatActivity {
     ImageView imagePreview;
     File userImageFolder;
     SimpleDateFormat dt;
+    SharedPreferences prefs;
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         this.imagePreview = (ImageView) findViewById(R.id.imagePreview);
         userImageFolder = getAlbumStorageDir(this, "Photos");
         dt = new SimpleDateFormat("yyyyymmdd_hhmmss");
+        prefs = getSharedPreferences("com.example.slaby.android_6", Context.MODE_PRIVATE);
+        String pathToOpen = prefs.getString("sciezka", null);
+        this.ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if (imagePreview.getTag() == null) {
+                    System.out.println("RETURN");
+                    return;
+                }
+                String path = imagePreview.getTag().toString();
+                prefs.edit().putFloat(path, rating).apply();
+            }
+        });
+        if (pathToOpen != null) {
+            setImage(pathToOpen);
+        }
+    }
+
+
+    public void setImage(String pathToOpen) {
+        if (prefs.contains(pathToOpen)) {
+            ratingBar.setRating( prefs.getFloat(pathToOpen, (float) 0));
+        }
+        File tmp = new File(pathToOpen);
+        Bitmap bitmap = BitmapFactory.decodeFile(tmp.getPath());
+        imagePreview.setTag(tmp.getAbsoluteFile());
+        imagePreview.setImageBitmap(bitmap);
+    }
+
+    public void onRatingClick(View view) {
+        System.out.println("onRatingClick" + view.toString());
     }
 
     @Override
